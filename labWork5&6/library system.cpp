2197b84d3949dc4c0cb7b2efeb72b156445970a3
maroosha-khushbakht <f2025-0721@bnu.edu.pk>
2025-11-14 09:52:58 +0500

    Refactor library system to calculate fines correctly

--- labWork5&6/library system.cpp
+++ labWork5&6/library system.cpp
@@ -2,25 +2,24 @@
 using namespace std;
 int main() 
 {
-    int nb;
-    cout << "Enter number of books: ";
+    int nb, nd, f=5, bf=0, tf=0;
+    cout << "Enter the number of books: ";
     cin >> nb;
-
-    int b = 1;
-    while (b <= nb) 
-    {
-        cout << "Book " << b << ":" << endl;
-        int d = 1;
-        int f = 5;
-        while (f <= 1000)
-        {
-            cout << "Day " << d << ": Rs. " << f << endl;
-            f *= 2;
-            d++;
+    
+    for (int i = 1; i <= nb; i++)
+    {   cout << "enter how late book " << i << " is: ";
+        cin >> nd;
+        for (int j = 1; j <= nd; j++)
+        {  bf += f;
+        if (bf >= 1000)
+        {   bf = 1000;
+            break;
         }
-        if (f > 1000) cout << "Fine reached max: Rs. 1000" << endl;
-        cout << endl;
-        b++;
+        f *= 2;
+        
+        }
+        tf+=f;
     }
+    cout << "total fine: Rs. " << tf;
     return 0;
 }
