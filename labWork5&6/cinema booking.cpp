fcd0c63c082ae10c058594bba42497e046328134
maroosha-khushbakht <f2025-0721@bnu.edu.pk>
2025-12-04 22:48:56 +0500

    Update cinema booking.cpp

--- labWork5&6/cinema booking.cpp
+++ labWork5&6/cinema booking.cpp
@@ -14,25 +14,42 @@ int main()
 		for (int j = 0; j < r; j++) 
 		{
 			arr[i][j] = 'F';
-			cout << "(" << i << " - " << j << " " << arr[i][j] << ") ";
+			cout << "(" << i + 1 << " - " << j + 1 << " " << arr[i][j] << ") ";
 		}
 		cout << endl;
 	}
-	int rc, cc;
-	cout << "Enter row of the seat number you want to reserve: ";
-	cin >> rc;
-	cout << "Enter column of the seat number you want to reserve: ";
-	cin >> cc;
-	arr[rc][cc] = 'T';
-	cout << "Updated Seat Reservation Status: " << endl;
-	for (int i = 0; i < c; i++) 
-	{
-		for (int j = 0; j < r; j++) 
+	int cc, rc;
+	cc = rc = 0;
+
+	
+	do {
+		cout << "Enter row of the seat number you want to reserve: ";
+		cin >> rc;
+		cout << "Enter column of the seat number you want to reserve: ";
+		cin >> cc;
+		arr[rc-1][cc-1] = 'R';
+		cout << "Updated Seat Reservation Status: " << endl;
+		if (cc<=c && rc<= r && arr[rc - 1][cc - 1] == 'R') 
 		{
-			cout << "(" << i << " - " << j << " " << arr[i][j] << ") ";
+			cout << "Seat (" << rc << " - " << cc << ") is successfully reserved." << endl;
 		}
-		cout << endl;
-	}
+		else if (arr[rc - 1][cc - 1] == 'F')
+		{
+			cout << "Seat (" << rc << " - " << cc << ") is already reserved." << endl;
+		}
+		else if (cc != 0 && rc != 0)
+		{
+			cout << "Invalid Seat Number." << endl;
+		}
+		for (int i = 0; i < c; i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				cout << "(" << i + 1 << " - " << j + 1 << " " << arr[i][j] << ") ";
+			}
+			cout << endl;
+		}
+	} while (cc != 0 && rc != 0);
 	return 0;
 
 }
