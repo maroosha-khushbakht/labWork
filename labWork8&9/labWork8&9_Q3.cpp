4a3c118382b1413972a0c4945c48ab1663f6e467
maroosha-khushbakht <f2025-0721@bnu.edu.pk>
2025-12-05 10:23:14 +0500

    Update labWork8&9_Q3.cpp

--- labWork8&9/labWork8&9_Q3.cpp
+++ labWork8&9/labWork8&9_Q3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[7];
+    int n = 7;
+    for (int i = 0; i < n; i++)
+    {
+         cout << "Enter number " << i + 1 << " of the array: ";
+         cin >> arr[i];
+    }
+   
+    int f = 0;
+    int l = n - 1;
 
+    while (f < l)
+    {
+        int temp = arr[f];
+        arr[f] = arr[l];
+        arr[l] = temp;
+
+        f++;
+        l--;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
