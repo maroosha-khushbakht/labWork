ac8aa1b30f3cfabe0e76fd0c939f5a246d01243d
maroosha-khushbakht <f2025-0721@bnu.edu.pk>
2025-12-05 09:34:15 +0500

    Update labWork8&9_Q1.cpp

--- labWork8&9/labWork8&9_Q1.cpp
+++ labWork8&9/labWork8&9_Q1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[7];
+    cout << "Enter 7 numbers:" << endl;
+
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int mx = arr[0];
+
+    for (int i = 1; i < 7; i++)
+    {
+        if (arr[i] > mx)
+        {
+            mx = arr[i];
+        }
+    }
+
+    cout << "Maximum number: " << mx << endl;
+
+    return 0;
+}
 
